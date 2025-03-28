a49df82f9f85eb31703734d07d64bd43f0b934a1
KSanders04 <kasanders2004@gmail.com>
2025-03-27 23:08:44 -0400

    Add to config for pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// https://KSanders04.github.io/Technical-Mockup
 export default defineConfig({
   plugins: [vue()],
 })
